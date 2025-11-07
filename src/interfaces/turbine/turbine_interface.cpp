c20dc2aea311c94720e3ff68726ab84eae227d6c
Michael Kuhn <mkuhn@nrel.gov>
2025-11-07 15:19:41 -0700

    one way to get output timing more available

--- src/interfaces/turbine/turbine_interface.cpp
+++ src/interfaces/turbine/turbine_interface.cpp
@@ -120,7 +120,7 @@ void TurbineInterface::UpdateAerodynamicLoads(
     }
 }
 
-bool TurbineInterface::Step() {
+bool TurbineInterface::Step(bool allow_output) {
     auto step_resion = Kokkos::Profiling::ScopedRegion("TurbineInterface::Step");
     // Update the host state with current node loads
     {
@@ -158,7 +158,7 @@ bool TurbineInterface::Step() {
     }
 
     // Write outputs and increment timestep counter
-    if (this->outputs) {
+    if (this->outputs && allow_output) {
         auto output_region = Kokkos::Profiling::ScopedRegion("Output Data");
         // Write node state outputs
         this->outputs->WriteNodeOutputsAtTimestep(this->host_state, this->state.time_step);
@@ -573,5 +573,29 @@ void TurbineInterface::ApplyController(double t) {
     this->turbine.blade_pitch_control[1] = controller->io.pitch_collective_command;
     this->turbine.blade_pitch_control[2] = controller->io.pitch_collective_command;
     this->turbine.yaw_control = controller->YawAngleCommand();
+}
+
+void TurbineInterface::CloseOutputFile()
+{
+    if (this->outputs) {
+        this->outputs->Close();
+    }
+}
+
+void TurbineInterface::OutputNow(int timestep)
+{
+    // Write outputs and increment timestep counter
+    if (this->outputs) {
+        this->outputs->Open();
+
+        auto output_region = Kokkos::Profiling::ScopedRegion("Output Data");
+        // Write node state outputs
+        this->outputs->WriteNodeOutputsAtTimestep(this->host_state, timestep);
+
+        // Calculate rotor azimuth and speed -> write rotor time-series data
+        this->WriteTimeSeriesData();
+
+        this->outputs->Close();
+    }
 }
 }  // namespace kynema::interfaces
--- src/interfaces/turbine/turbine_interface.hpp
+++ src/interfaces/turbine/turbine_interface.hpp
@@ -85,7 +85,7 @@ public:
      *       solves the dynamic system, and updates the node motion with the new state.
      *       If the solver does not converge, the motion is not updated.
      */
-    [[nodiscard]] bool Step();
+    [[nodiscard]] bool Step(bool allow_output = true);
 
     /// @brief Saves the current state for potential restoration (in correction step)
     void SaveState();
@@ -110,6 +110,10 @@ public:
      * @return Rotor speed in rad/s
      */
     [[nodiscard]] double CalculateRotorSpeed() const;
+
+    void OutputNow(int timestep);
+
+    void CloseOutputFile();
 
 private:
     Model model;                    ///< Kynema class for model construction
