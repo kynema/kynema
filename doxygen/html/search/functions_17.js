9ea3f3f3d40274965ebe4718a9acc823e46863b9
deslaughter <deslaughter@users.noreply.github.com>
2025-12-04 16:04:49 +0000

    deploy: eadd92f109f08a01bf9f6105c45750670f02695c

--- doxygen/html/search/functions_17.js
+++ doxygen/html/search/functions_17.js
@@ -3,5 +3,7 @@ var searchData=
   ['_7edylib_0',['~dylib',['../classkynema_1_1util_1_1dylib.html#a19d9d672a13565419b817740b4fd2ca3',1,'kynema::util::dylib']]],
   ['_7enetcdffile_1',['~NetCdfFile',['../classkynema_1_1util_1_1NetCdfFile.html#ad9057f7f4e54200f1c0d29d6bc2a05a2',1,'kynema::util::NetCdfFile']]],
   ['_7enodebuilder_2',['~NodeBuilder',['../classkynema_1_1NodeBuilder.html#aa16b8f2af8bb0e9c8dbd4598fbbf81f1',1,'kynema::NodeBuilder']]],
-  ['_7enodestatewriter_3',['~NodeStateWriter',['../classkynema_1_1util_1_1NodeStateWriter.html#a4dead80eedc71ba7e6ca55594f9940c4',1,'kynema::util::NodeStateWriter']]]
+  ['_7enodestatewriter_3',['~NodeStateWriter',['../classkynema_1_1util_1_1NodeStateWriter.html#a4dead80eedc71ba7e6ca55594f9940c4',1,'kynema::util::NodeStateWriter']]],
+  ['_7eturbine_4',['~Turbine',['../classkynema_1_1interfaces_1_1components_1_1Turbine.html#a5a9caf85845adcddaea1904c80360ef0',1,'kynema::interfaces::components::Turbine']]],
+  ['_7eturbineinterface_5',['~TurbineInterface',['../classkynema_1_1interfaces_1_1TurbineInterface.html#ad01665ba02dbd612e6df168781e813ad',1,'kynema::interfaces::TurbineInterface']]]
 ];
