7053bffcf978df14d80f0aaff6497ed306c1fc0c
Derek Slaughter <deslaughter@gmail.com>
2026-03-01 14:31:09 -0800

    Re-enable build matrices for GitHub Actions CI (#541)
    * Disable null-pointer warning which was causing a compilation failure
    with g++ on release build in CI
    
    * Update deploy_docs.yaml

--- cmake/CompilerWarnings.cmake
+++ cmake/CompilerWarnings.cmake
@@ -1,8 +1,8 @@
 function(kynema_set_project_warnings project_name WARNINGS_AS_ERRORS CLANG_WARNINGS GCC_WARNINGS CUDA_WARNINGS)
 
-  # Set default Clang warnings if not provided
-  if("${CLANG_WARNINGS}" STREQUAL "")
-    set(CLANG_WARNINGS
+    # Set default Clang warnings if not provided
+    if("${CLANG_WARNINGS}" STREQUAL "")
+        set(CLANG_WARNINGS
       -Wall               # Enable all standard warnings
       -Wextra             # Enable extra warnings
       -Wshadow            # Warn if a variable declaration shadows one from a parent context
@@ -13,16 +13,16 @@ function(kynema_set_project_warnings project_name WARNINGS_AS_ERRORS CLANG_WARNI
       -Wpedantic          # Warn if non-standard C++ is used
       -Wconversion        # Warn on type conversions that may lose data
       -Wsign-conversion   # Warn on sign conversions
-      -Wnull-dereference  # Warn if a null dereference is detected
+      # -Wnull-dereference  # Warn if a null dereference is detected (disabled due to false positive in g++)
       -Wdouble-promotion  # Warn if float is implicitly promoted to double
       -Wformat=2          # Warn on security issues around functions that format output (e.g., printf)
       -Wimplicit-fallthrough # Warn on statements that fall through without an explicit annotation
     )
-  endif()
+    endif()
 
-  # Set default GCC warnings if not provided
-  if("${GCC_WARNINGS}" STREQUAL "")
-    set(GCC_WARNINGS
+    # Set default GCC warnings if not provided
+    if("${GCC_WARNINGS}" STREQUAL "")
+        set(GCC_WARNINGS
       ${CLANG_WARNINGS}         # Include Clang warnings
       -Wmisleading-indentation  # Warn if indentation implies blocks where blocks do not exist
       -Wduplicated-cond         # Warn if an if/else chain has duplicated conditions
@@ -31,40 +31,40 @@ function(kynema_set_project_warnings project_name WARNINGS_AS_ERRORS CLANG_WARNI
       -Wuseless-cast            # Warn if you perform a cast to the same type
       -Wsuggest-override        # Warn if an overridden member function is not marked 'override' or 'final'
     )
-  endif()
+    endif()
 
-  # Set default CUDA warnings if not provided
-  if("${CUDA_WARNINGS}" STREQUAL "")
-    set(CUDA_WARNINGS
+    # Set default CUDA warnings if not provided
+    if("${CUDA_WARNINGS}" STREQUAL "")
+        set(CUDA_WARNINGS
       -Wall
       -Wextra
       -Wunused
       -Wconversion
       -Wshadow
     )
-  endif()
+    endif()
 
-  # Treat warnings as errors if specified
-  if(WARNINGS_AS_ERRORS)
-    message(STATUS "Warnings are treated as errors for project '${project_name}'")
-    list(APPEND CLANG_WARNINGS -Werror)
-    list(APPEND GCC_WARNINGS -Werror)
-    list(APPEND CUDA_WARNINGS -Werror)
-  endif()
+    # Treat warnings as errors if specified
+    if(WARNINGS_AS_ERRORS)
+        message(STATUS "Warnings are treated as errors for project '${project_name}'")
+        list(APPEND CLANG_WARNINGS -Werror)
+        list(APPEND GCC_WARNINGS -Werror)
+        list(APPEND CUDA_WARNINGS -Werror)
+    endif()
 
-  # Set compiler-specific warning flags
-  if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-    set(PROJECT_WARNINGS_CXX ${CLANG_WARNINGS})
-  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    set(PROJECT_WARNINGS_CXX ${GCC_WARNINGS})
-  else()
-    message(WARNING "No compiler warnings set for CXX compiler: '${CMAKE_CXX_COMPILER_ID}'")
-  endif()
+    # Set compiler-specific warning flags
+    if(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        set(PROJECT_WARNINGS_CXX ${CLANG_WARNINGS})
+    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+        set(PROJECT_WARNINGS_CXX ${GCC_WARNINGS})
+    else()
+        message(WARNING "No compiler warnings set for CXX compiler: '${CMAKE_CXX_COMPILER_ID}'")
+    endif()
 
-  set(PROJECT_WARNINGS_CUDA "${CUDA_WARNINGS}")
+    set(PROJECT_WARNINGS_CUDA "${CUDA_WARNINGS}")
 
-  # Apply the warning flags to the target
-  target_compile_options(
+    # Apply the warning flags to the target
+    target_compile_options(
     ${project_name}
     INTERFACE
     $<$<COMPILE_LANGUAGE:CXX>:${PROJECT_WARNINGS_CXX}>
--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -1,16 +1,16 @@
 macro(kynema_enable_cppcheck WARNINGS_AS_ERRORS CPPCHECK_OPTIONS)
-  find_program(CPPCHECK cppcheck)
-  if(CPPCHECK)
-    set(CPPCHECK_TEMPLATE "gcc")
-    if(CMAKE_GENERATOR MATCHES ".*Visual Studio.*")
-      set(CPPCHECK_TEMPLATE "vs")
-    endif()
+    find_program(CPPCHECK cppcheck)
+    if(CPPCHECK)
+        set(CPPCHECK_TEMPLATE "gcc")
+        if(CMAKE_GENERATOR MATCHES ".*Visual Studio.*")
+            set(CPPCHECK_TEMPLATE "vs")
+        endif()
 
-    # Default cppcheck options if none are provided
-    if("${CPPCHECK_OPTIONS}" STREQUAL "")
-      set(SUPPRESS_DIR "*:${CMAKE_CURRENT_BINARY_DIR}/_deps/*.h")
-      message(STATUS "CPPCHECK suppressing warnings for directory: ${SUPPRESS_DIR}")
-      set(CMAKE_CXX_CPPCHECK
+        # Default cppcheck options if none are provided
+        if("${CPPCHECK_OPTIONS}" STREQUAL "")
+            set(SUPPRESS_DIR "*:${CMAKE_CURRENT_BINARY_DIR}/_deps/*.h")
+            message(STATUS "CPPCHECK suppressing warnings for directory: ${SUPPRESS_DIR}")
+            set(CMAKE_CXX_CPPCHECK
         ${CPPCHECK}
         --template=${CPPCHECK_TEMPLATE}
         --enable=style,performance,warning,portability
@@ -24,60 +24,60 @@ macro(kynema_enable_cppcheck WARNINGS_AS_ERRORS CPPCHECK_OPTIONS)
         --suppress=syntaxError # Suppress syntax errors in the code
         --suppress=preprocessorErrorDirective # Suppress preprocessor error directives
         --suppress=${SUPPRESS_DIR})
-      else()
-        # If the user provides a CPPCHECK_OPTIONS with a template specified, use it
-        set(CMAKE_CXX_CPPCHECK ${CPPCHECK} --template=${CPPCHECK_TEMPLATE} ${CPPCHECK_OPTIONS})
-    endif()
+        else()
+            # If the user provides a CPPCHECK_OPTIONS with a template specified, use it
+            set(CMAKE_CXX_CPPCHECK ${CPPCHECK} --template=${CPPCHECK_TEMPLATE} ${CPPCHECK_OPTIONS})
+        endif()
 
-    if(NOT "${CMAKE_CXX_STANDARD}" STREQUAL "")
-      set(CMAKE_CXX_CPPCHECK ${CMAKE_CXX_CPPCHECK} --std=c++${CMAKE_CXX_STANDARD})
-    endif()
+        if(NOT "${CMAKE_CXX_STANDARD}" STREQUAL "")
+            set(CMAKE_CXX_CPPCHECK ${CMAKE_CXX_CPPCHECK} --std=c++${CMAKE_CXX_STANDARD})
+        endif()
 
-    if(${WARNINGS_AS_ERRORS})
-      list(APPEND CMAKE_CXX_CPPCHECK --error-exitcode=2)
-    endif()
+        if(${WARNINGS_AS_ERRORS})
+            list(APPEND CMAKE_CXX_CPPCHECK --error-exitcode=2)
+        endif()
 
-  else()
-    message(WARNING "cppcheck requested but executable not found")
-  endif()
+    else()
+        message(WARNING "cppcheck requested but executable not found")
+    endif()
 endmacro()
 
 macro(kynema_enable_clang_tidy target WARNINGS_AS_ERRORS)
-  find_program(CLANGTIDY clang-tidy)
-  if(CLANGTIDY)
-    if(NOT CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
-      get_target_property(TARGET_PCH ${target} INTERFACE_PRECOMPILE_HEADERS)
-      if("${TARGET_PCH}" STREQUAL "TARGET_PCH-NOTFOUND")
-        get_target_property(TARGET_PCH ${target} PRECOMPILE_HEADERS)
-      endif()
+    find_program(CLANGTIDY clang-tidy)
+    if(CLANGTIDY)
+        if(NOT CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+            get_target_property(TARGET_PCH ${target} INTERFACE_PRECOMPILE_HEADERS)
+            if("${TARGET_PCH}" STREQUAL "TARGET_PCH-NOTFOUND")
+                get_target_property(TARGET_PCH ${target} PRECOMPILE_HEADERS)
+            endif()
 
-      if(NOT ("${TARGET_PCH}" STREQUAL "TARGET_PCH-NOTFOUND"))
-        message(FATAL_ERROR "clang-tidy cannot be used with non-Clang compilers and PCH (Precompiled Headers).")
-      endif()
-    endif()
+            if(NOT ("${TARGET_PCH}" STREQUAL "TARGET_PCH-NOTFOUND"))
+                message(FATAL_ERROR "clang-tidy cannot be used with non-Clang compilers and PCH (Precompiled Headers).")
+            endif()
+        endif()
 
-    set(CLANG_TIDY_OPTIONS
+        set(CLANG_TIDY_OPTIONS
       ${CLANGTIDY}
       -extra-arg=-Wno-unknown-warning-option # Ignore unknown warning options in Clang-Tidy
       -extra-arg=-Wno-ignored-optimization-argument # Ignore ignored optimization arguments
       -extra-arg=-Wno-unused-command-line-argument # Ignore unused command line arguments
       -p)
 
-    if(NOT "${CMAKE_CXX_STANDARD}" STREQUAL "")
-      if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
-        list(APPEND CLANG_TIDY_OPTIONS -extra-arg=-std=c++${CMAKE_CXX_STANDARD})
-      else()
-        list(APPEND CLANG_TIDY_OPTIONS -extra-arg=/std:c++${CMAKE_CXX_STANDARD})
-      endif()
-    endif()
+        if(NOT "${CMAKE_CXX_STANDARD}" STREQUAL "")
+            if(CMAKE_CXX_COMPILER_ID MATCHES ".*Clang")
+                list(APPEND CLANG_TIDY_OPTIONS -extra-arg=-std=c++${CMAKE_CXX_STANDARD})
+            else()
+                list(APPEND CLANG_TIDY_OPTIONS -extra-arg=/std:c++${CMAKE_CXX_STANDARD})
+            endif()
+        endif()
 
-    if(${WARNINGS_AS_ERRORS})
-      list(APPEND CLANG_TIDY_OPTIONS -warnings-as-errors=*)
-    endif()
+        if(${WARNINGS_AS_ERRORS})
+            list(APPEND CLANG_TIDY_OPTIONS -warnings-as-errors=*)
+        endif()
 
-    message(STATUS "Setting clang-tidy globally for target: ${target}")
-    set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_OPTIONS})
-  else()
-    message(WARNING "clang-tidy requested but executable not found")
-  endif()
+        message(STATUS "Setting clang-tidy globally for target: ${target}")
+        set(CMAKE_CXX_CLANG_TIDY ${CLANG_TIDY_OPTIONS})
+    else()
+        message(WARNING "clang-tidy requested but executable not found")
+    endif()
 endmacro()
